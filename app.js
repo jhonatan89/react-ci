5d4cd0f1cb89cefcdcfdd60f755592157ccf815b
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2021-03-15 13:46:29 -0500

    Add build step

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("build"));
+app.use(express.static('build'));
 
 app.listen(PORT, () => {
-  console.log("Server started on port 5000");
+  console.log('Server started on port 5000');
 });
